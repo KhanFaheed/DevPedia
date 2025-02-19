ea44e99079316c99b9ba8e642544af2b6815ab88
KhanFaheed <faheedk215@gmail.com>
2025-02-19 11:09:32 +0300

    Added the models Profile,Review and Tags

--- projects/models.py
+++ projects/models.py
@@ -8,12 +8,47 @@ class Project(models.Model):
      #null=true i.e that we are allowed to have this value empty in the database {null-->related to datbase}  blank=True i.e form can have this field as empty
     demo_link=models.CharField(max_length=2000,null=True,blank=True)
     source_link=models.CharField(max_length=2000,null=True,blank=True)
+    tags=models.ManyToManyField('Tag',blank=True)
+    vote_total=models.IntegerField(default=0,null=True,blank=True)
+    vote_ratio=models.IntegerField(default=0,null=True,blank=True)
     created=models.DateTimeField(auto_now_add=True)
     id=models.UUIDField(default=uuid.uuid4,unique=True,
                         primary_key=True,editable=False)
     
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+     VOTE_TYPE=(
+        ('Up','Up vote'),
+        ('Down','Down vote')
+    )
+     #owner
+
+     #Note->foreign key is created on the many side model
+     project=models.ForeignKey(Project,on_delete=models.CASCADE)
+     body=models.TextField(null=True,blank=True)
+     value=models.CharField(max_length=200,choices=VOTE_TYPE)
+     created=models.DateTimeField(auto_now_add=True)
+     id=models.UUIDField(default=uuid.uuid4,unique=True,
+                        primary_key=True,editable=False)
+     
+     def __str__(self):
+         return self.value
+
+class Tag(models.Model):
+    name=models.CharField(max_length=200)
+    created=models.DateTimeField(auto_now_add=True)
+    id=models.UUIDField(default=uuid.uuid4,unique=True,
+                        primary_key=True,editable=False)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+    
+     
     
     
 
--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Project,Tag,Review
 def projects(request):
-    return render(request,'projects/projects.html')
+    projects=Project.objects.all()
+    context={
+        'projects':projects
+    }
+    return render(request,'projects/projects.html',context)
+
 
 def project(request,pk):
-    return render(request,'projects/single-project.html')
+    projectobj=Project.objects.get(id=pk)
+    context={'project':projectobj}
+    return render(request,'projects/single-project.html',context)
+
+def createProject(request):
+    return render(request,'projects/project_form.html')
