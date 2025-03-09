9cbdbb3b8800da9e92b4672acbc5e8b6a8d70178
faheed khan <faheedk215@gmail.com>
2025-03-09 10:14:37 +0200

    added the profile of each user

--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     name=models.CharField(max_length=200,blank=True,null=True)
     email=models.EmailField(max_length=500,blank=True,null=True)
     username=models.CharField(max_length=200,blank=True,null=True)
+    location=models.CharField(max_length=200,blank=True,null=True)
     short_intro=models.CharField(max_length=200,blank=True,null=True)
     bio=models.TextField(blank=True,null=True)
     Profile_image=models.ImageField(null=True,blank=True,upload_to='profiles/',default='profiles/user-default.png')
@@ -22,5 +23,26 @@ class Profile(models.Model):
                         primary_key=True,editable=False)
     def __str__(self):
         return str(self.user.username)
+    
+class Skill(models.Model):
+    owner=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)#foreign key
+    name=models.CharField(max_length=200,blank=True,null=True)
+    desription=models.TextField(null=True,blank=True)
+    created=models.DateTimeField(auto_now_add=True)
+    id=models.UUIDField(default=uuid.uuid4,unique=True,
+                        primary_key=True,editable=False)
+    def __str__(self):
+        return self.name
 
+    """1. Understanding the Reverse Relationship
 
+     When you define owner = models.ForeignKey(Profile, ...) in the Skill model,
+     Django creates a reverse relationship on the Profile model. 
+     By default, the name of this reverse relationship is skill_set (the model name in lowercase, followed by _set).
+
+     2. Accessing Skills from a Profile Object
+     To access the skills associated with a specific Profile object,
+     profile=Profile.object.filter(name='Faheed Khan')        ->profile object
+     you can use profile.skill_set.all(). This will return a
+     queryset containing all Skill objects that have the given Profile as their owner.
+   """
